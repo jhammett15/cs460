0810bf1279142d138bbb41fd78e4e48f543c98a8
Jerika <jhammett15@mail.wou.edu>
2018-10-12 04:26:26 -0700

    translated QueueInterface java file into IQueue C# file

--- HWK3/HWK3/IQueue.cs
+++ HWK3/HWK3/IQueue.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace HWK3
 {
-    class IQueue
+    public interface IQueue<T>
     {
+        // adds an element to the rear of the queue
+        // returns the element that was enqueued
+        T Push(T element);
+
+        // removes and returns the front element
+        // exception thrown if the queue is empty
+        T Pop();
+
+        // test if the queue is empty
+        // return true if the queue is empty, otherwise false
+        bool IsEmpty();
     }
 }
