dccd0f869a03b03948cdae3e98f0f900587fbe3a
Jerika <jhammett15@mail.wou.edu>
2018-10-12 21:31:42 -0700

    translated QueueUnderflowException

--- HWK3/HWK3/QueueUnderflowException.cs
+++ HWK3/HWK3/QueueUnderflowException.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace HWK3
 {
-    class QueueUnderflowException
+    public class QueueUnderflowException
     {
+        // empty constructor, creates an exception without a message
+        public QueueUnderflowException()
+        {
+        }
+
+        // prints out the message taken in then the stacktrace
+        public QueueUnderflowException(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(Environment.StackTrace);
+        }
     }
 }
