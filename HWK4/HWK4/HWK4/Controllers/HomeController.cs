9223d9c822f8bea61e606e1e8f2648b3393873a7
Jerika <jhammett15@mail.wou.edu>
2018-10-22 08:54:04 -0700

    did the layout for the mile converter page and got the query string looking right

--- HWK4/HWK4/HWK4/Controllers/HomeController.cs
+++ HWK4/HWK4/HWK4/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,18 +14,43 @@ namespace HWK4.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult MileConverter()
         {
-            ViewBag.Message = "Your application description page.";
+            double mi = Convert.ToDouble(Request.QueryString["miles"]);
+            string measurement = Request.QueryString["units"];
+            double total;
+            string result;
+
+            if(measurement == "millimeters")
+            {
+                total = mi * 1609.34 * 1000;
+            }
+            else
+            {
+                if(measurement == "centimeters")
+                {
+                    total = mi * 1609.34 * 100;
+                }
+                else
+                {
+                    if(measurement == "meters")
+                    {
+                        total = mi * 1609.34;
+                    }
+                    else
+                    {
+                        total = mi * 1.60934;
+                    }
+                }
+            }
+
+            if(mi != null && measurement != null)
+            {
+                result = mi + " miles is equal to " + total + " " + measurement + ".";
+            }
 
             return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        }        
     }
 }
