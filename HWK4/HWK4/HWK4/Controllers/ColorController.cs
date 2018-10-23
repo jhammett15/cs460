64f834193d794c27f64b44708ec2a87521a01cd7
Jerika <jhammett15@mail.wou.edu>
2018-10-23 11:18:09 -0700

    added xml comments and non-xml comments

--- HWK4/HWK4/HWK4/Controllers/ColorController.cs
+++ HWK4/HWK4/HWK4/Controllers/ColorController.cs
@@ -5,23 +5,32 @@ using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using HWK4.Models;
 
 namespace HWK4.Controllers
 {
+    /// <summary>
+    /// A Controller class for the Color Converter page. The purpose of this page is to take in two colors from the user via a POST method, 
+    /// add their hexadecimal values together, and return this new value.
+    /// </summary>
     public class ColorController : Controller
     {
+        /// <summary>
+        /// Controller method that generates the Color Converter page
+        /// </summary>
+        /// <returns>the view for the color converter page</returns>
         public ActionResult Index()
         {
             return View();
         }
         
-        [HttpPost]
-        public ActionResult Create()
-        {
-            return View();
-        }
-
+        /// <summary>
+        /// Takes in two colors, converts them to hexadecimal, then uses them to render a third color. Then adds these colors to the 
+        /// ViewBag and returns the View. The Debug.WriteLines are unnecessary to the view, but useful to see that the correct hexadecimal
+        /// numbers are being returned.
+        /// </summary>
+        /// <param name="firstColor">A Color object, the first object chosen by the user</param>
+        /// <param name="secondColor">A Color object, the second object chosen by the user</param>
+        /// <returns>The View for the Create page</returns>
         [HttpPost]
         public ActionResult Create(Color firstColor, Color secondColor)
         {
@@ -40,6 +49,13 @@ namespace HWK4.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Takes in two colors, separates them into their RGB components and adds these components together. If any of the components' 
+        /// values are over 255 (max), then they default to 255. Then combines these separate RGB components into a hexadecimal string
+        /// </summary>
+        /// <param name="color1">A Color object, one of two that will be combined</param>
+        /// <param name="color2">A Color object, one of two that will be combined.</param>
+        /// <returns>A string representing the hexadecimal value of the two colors added together</returns>
         public string ColorAdd(Color color1, Color color2)
         {
             int r = color1.R + color2.R;
@@ -54,8 +70,7 @@ namespace HWK4.Controllers
                 b = 255;
 
             string colorhex = r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
-
-
+            
             return colorhex;
         }
     }
--- HWK4/HWK4/HWK4/Controllers/HomeController.cs
+++ HWK4/HWK4/HWK4/Controllers/HomeController.cs
@@ -7,17 +7,33 @@ using System.Web.Mvc;
 
 namespace HWK4.Controllers
 {
+    /// <summary>
+    /// A Controller for the Home page and the Mile Converter page. The home page, also called the Index page, is the landing site for the 
+    /// project. It has links to both the Mile Converter page and the Color Chooser page. The Mile Converter page takes in a number 
+    /// from the user as well as a radio button value via a HttpGet method and computes a mile conversion.
+    /// </summary>
     public class HomeController : Controller
     {
         private double? mi;
         private string measurement;
         private string result;
 
+        /// <summary>
+        /// A Controller method that generates the "Home" or "Index" page of the project
+        /// </summary>
+        /// <returns>The View for the Index page.</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// A Controller method that retrieves a double representing the number of miles and a string representing the measurement to be 
+        /// converted to from the View via a GET method. It then converts the miles, storing the result in a new variable. Finally it checks 
+        /// so see if the measurement variable is not null. If it is null, this means that the input has not been submitted yet. If it is 
+        /// not null, the input has ben submitted and it stores the measurement variable and result of the conversion in a ViewBag.
+        /// </summary>
+        /// <returns>The View for the Mile Converter page</returns>
         [HttpGet]
         public ActionResult Converter()
         {
@@ -63,8 +79,6 @@ namespace HWK4.Controllers
             }
 
             return View();
-        }
-
-       
+        }       
     }
 }
