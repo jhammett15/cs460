f66ad22279c344d56eb35ed7fa0a899f4fd521a2
Jerika <jhammett15@mail.wou.edu>
2018-10-29 12:35:31 -0700

    added more constraints to Model, minor tweaks to index page, and took screenshots of running program for blog page

--- HWK5/HWK5/Models/Request.cs
+++ HWK5/HWK5/Models/Request.cs
@@ -13,24 +13,31 @@ namespace HWK5.Models
 
         [Required(ErrorMessage = "Please enter your first name")]
         [StringLength(15)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Please enter your last name")]
         [StringLength(15)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Please enter your ten digit phone number")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^(\d{10})$")]
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Please enter your apartment name")]
+        [StringLength(30)]
+        [Display(Name = "Apartment Name")]
         public string ApartmentName { get; set; }
 
         [Required(ErrorMessage = "Please enter your unit number")]
+        [Display(Name = "Phone Number")]
         public int UnitNumber { get; set; }
 
         [Required(ErrorMessage = "Please enter your request")]
+        [StringLength(250)]
         public string Explanation { get; set; }
 
         [Required]
